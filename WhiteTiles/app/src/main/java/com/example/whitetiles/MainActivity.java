c2fc89df07b630012aff27ef5728e7ab501bb51b
Dominte Florin <dominte.florin.iulian@gmail.com>
2021-05-03 13:40:58 +0300

    Shared Preferences

--- WhiteTiles/app/src/main/java/com/example/whitetiles/MainActivity.java
+++ WhiteTiles/app/src/main/java/com/example/whitetiles/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.whitetiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int numberOfTiles= 3 ;
+    private String difficulty = "Slow";
+
+
+    public void openOptionsActivity(){
+        Intent intent = new Intent(this,OptionsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlayActivity(){
+        Intent intent = new Intent(this, PlayActivity.class);
+        intent.putExtra("EXTRA_DIFFICULTY",difficulty);
+        intent.putExtra("EXTRA_NUMBER_OF_TILES",numberOfTiles);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button optionButton = (Button) findViewById(R.id.optionButton);
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOptionsActivity();
+            }
+        });
+
+        Button playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPlayActivity();
+            }
+        });
     }
 }
--- WhiteTiles/app/src/main/java/com/example/whitetiles/OptionsActivity.java
+++ WhiteTiles/app/src/main/java/com/example/whitetiles/OptionsActivity.java
@@ -1,4 +1,18 @@
 package com.example.whitetiles;
 
-public class OptionsActivity {
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OptionsActivity extends PreferenceActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getPreferenceManager().setSharedPreferencesName("my_prefs");
+        addPreferencesFromResource(R.xml.options);
+    }
 }
