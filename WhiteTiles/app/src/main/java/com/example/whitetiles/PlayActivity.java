9dd30d820cf0bd796a576f49bcc7f20b208adc6d
Dominte Florin <dominte.florin.iulian@gmail.com>
2021-05-03 17:09:17 +0300

    Shared preferences retrieve info

--- WhiteTiles/app/src/main/java/com/example/whitetiles/PlayActivity.java
+++ WhiteTiles/app/src/main/java/com/example/whitetiles/PlayActivity.java
@@ -1,5 +1,6 @@
 package com.example.whitetiles;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayActivity extends AppCompatActivity {
 
+    private int numberOfTiles; // 3 / 4 / 5
+    private int startingSpeed; // 0 slow / 1 fast
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -15,11 +19,17 @@ public class PlayActivity extends AppCompatActivity {
 
         Button startButton = (Button) findViewById(R.id.startButton);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("my_prefs",MODE_PRIVATE);
+        numberOfTiles = Integer.parseInt(sharedPreferences.getString("list_tiles",""));
+        startingSpeed = Integer.parseInt(sharedPreferences.getString("list_speed",""));
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(startButton.getVisibility() == View.VISIBLE)
-                startButton.setVisibility(View.GONE);
+                if(startButton.getVisibility() == View.VISIBLE) {
+                    startButton.setVisibility(View.GONE);
+                }
+
 
             }
         });
